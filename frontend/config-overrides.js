0eff4cf1522546af2a348c990568ff21f68cfda9
Jehy <fate@jehy.ru>
2024-02-02 18:41:08 +0300

    try fix inline

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -7,12 +7,28 @@ module.exports = aliasWebpack(options)
 module.exports.jest = aliasJest(options)
 */
 
+/*
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 
 module.exports = function override(config, env) {
     if (env === 'production') {
         config.plugins.push(new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/\.(js|css)$/]))
+    }
+    return config
+}
+*/
+
+
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin')
+
+module.exports = function override(config, env) {
+    if (env === 'production') {
+        config.plugins.push(new ScriptExtHtmlWebpackPlugin({
+            inline: /.+[.]js/
+        }))
+        config.plugins.push(new StyleExtHtmlWebpackPlugin())
     }
     return config
 }
