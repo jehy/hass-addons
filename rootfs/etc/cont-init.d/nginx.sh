54191496b7623be878ef5a545cf7bff32e2e7cf8
Jehy <fate@jehy.ru>
2024-02-04 19:11:05 +0300

    retry

--- rootfs/etc/cont-init.d/nginx.sh
+++ rootfs/etc/cont-init.d/nginx.sh
@@ -7,6 +7,8 @@ ingress_entry=$(bashio::addon.ingress_entry)
 ingress_interface=$(bashio::addon.ip_address)
 dns_host=$(bashio::dns.host)
 
+[[ $ingress_entry =~ ^/(.*)/$ ]] && ingress_entry=${BASH_REMATCH[1]} #Remove slashes from both sides
+
 # Retrieve the ingress_entry query path so that nginx can perform rewrites accordingly
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/nginx.conf
