310cb90f41a0aceae23b9ef1b9b020e86b96b358
Jehy <fate@jehy.ru>
2024-02-05 05:37:30 +0300

    fix

--- dbstats/backend/src/database.module.ts
+++ dbstats/backend/src/database.module.ts
@@ -1,18 +1,17 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from './config.service';
+import config from './config';
 import { TypeOrmLoggerContainer } from './typeOrmLogger';
 import { Module } from '@nestjs/common';
 
-const config = new ConfigService().data;
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      ...config.typeOrmConfig,
+      ...config().typeOrmConfig,
       synchronize: false,
       entities: [__dirname + '/entities/homeass/2024.1.5/*{.ts,.js}'],
       logger: TypeOrmLoggerContainer.ForConnection(
         'homeass',
-        config.db.logging || ['error'],
+        config().db.logging || ['error'],
       ),
       name: 'homeass',
       maxQueryExecutionTime: 1000,
