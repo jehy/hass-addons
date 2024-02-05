4e1c45408c4d559a331acf0dd9c27191d8e4ccea
Jehy <fate@jehy.ru>
2024-02-05 05:14:47 +0300

    fix

--- dbstats/backend/src/config.service.ts
+++ dbstats/backend/src/config.service.ts
@@ -146,9 +146,14 @@ postgresql://@/DB_NAME?host=/path/to/dir
       //const tmpFile = '/tmp/copy.db';
       const tmpFileRecovered = '/tmp/copy.recovered.db';
       //childProcess.execSync(`cp ${database} ${tmpFile}`);
+      try{
       childProcess.execSync(
-        `sqlite3 ${database} ".recover" | sqlite3 ${tmpFileRecovered}`,
-      );
+        `sqlite3 ${database} ".clone" | sqlite3 ${tmpFileRecovered}`,
+      ); }
+      catch(err){
+        console.log(err);
+        childProcess.execSync(`sqlite3 ${database} | sqlite3 ${tmpFileRecovered}`);
+      }
       const options: SqliteConnectionOptions = {
         type: 'sqlite',
         database: tmpFileRecovered,
